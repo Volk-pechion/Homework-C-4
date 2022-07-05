3b125733d3e542477509101ef7d805b58502d0ab
AndreyVolkov <volk_bur-gora@mail.ru>
2022-07-05 22:06:10 +0300

    Задание 1

---
+++ Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 1: Написать циклб который принимает на вход два чмсла (А и В)
+// и возводит число А в натуральную степень В
+
+int promt(string message)
+{
+    System.Console.WriteLine(message);
+    string param = Console.ReadLine();
+    return int.Parse(param);
+}
+
+int A = promt("Введите число A");
+int B = promt("введите число B");
+int count = 0;
+int DegreeA = 1;
+
+while (count<B)
+{
+    count++;
+    System.Console.WriteLine(DegreeA=DegreeA*A);
+}
