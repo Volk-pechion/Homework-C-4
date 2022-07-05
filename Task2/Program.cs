bcc23b16c9f5b15b97460b5d089f62a653c41da4
AndreyVolkov <volk_bur-gora@mail.ru>
2022-07-05 22:03:37 +0300

    Задание 2

---
+++ Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 2: Написать программу, которая принимает на вход число и выдает сумму цифр в числе
+
+int promt(string message)
+{
+   System.Console.Write(message); 
+   string param = Console.ReadLine();
+   return int.Parse(param);
+}
+
+int number = promt("Ведите число: ");
+int summa = 0; 
+
+while(number!=0)
+{
+    summa = summa + number % 10;
+    number = number / 10;
+}
+System.Console.WriteLine($"Сумма чисел равна: {summa}");
